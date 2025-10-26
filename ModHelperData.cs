ab0688f38cbf888996db63f3244dbfc39bad8ab1
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2025-10-26 19:37:31 +0100

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace AllTack;
 public static class ModHelperData
 {
     public const string Name = "All Tack";
-    public const string Description = "Makes towers projectiles fire like SuperMaelstrom. Remake from an old mod by Timotheeee.";
+    public const string Description = "Makes towers projectiles fire like SuperMaelstrom.";
     public const string Version = "1.0.0";
     public const string RepoOwner = "Myself-Bloons";
     public const string RepoName = "AllTack";
